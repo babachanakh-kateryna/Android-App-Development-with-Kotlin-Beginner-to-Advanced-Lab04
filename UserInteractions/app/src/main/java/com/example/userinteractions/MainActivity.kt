d7862e1a5ca2569e1b92a2973972787288668efb
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-17 00:21:17 +0100

    Snackbar Messages

--- UserInteractions/app/src/main/java/com/example/userinteractions/MainActivity.kt
+++ UserInteractions/app/src/main/java/com/example/userinteractions/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.userinteractions
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var showToast: Button
+    lateinit var showSnackbar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,9 +24,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         showToast = findViewById(R.id.btn)
+        showSnackbar = findViewById(R.id.btn2)
 
         showToast.setOnClickListener() {
             Toast.makeText(applicationContext, "This is a toast message", Toast.LENGTH_LONG).show()
+        }
+
+        showSnackbar.setOnClickListener() {
+            Snackbar.make(findViewById(R.id.main), "This is a snackbar message", Snackbar.LENGTH_SHORT).setAction("Close", View.OnClickListener {
+
+            })
+                .show()
         }
     }
 }
