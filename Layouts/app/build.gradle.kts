656fb9b61c6db34d0c7994b5b8113f6e9c14bdfb
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-16 20:12:03 +0100

    57. Textviews in Android App Development

--- Layouts/app/build.gradle.kts
+++ Layouts/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.layouts"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.layouts"
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.ui.graphics.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.layouts
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myText : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        myText = findViewById(R.id.textEx)
+        myText.setTextColor(Color.RED)
+
+        myText.setOnClickListener {
+            myText.setTextColor(Color.BLUE)
         }
     }
 }
