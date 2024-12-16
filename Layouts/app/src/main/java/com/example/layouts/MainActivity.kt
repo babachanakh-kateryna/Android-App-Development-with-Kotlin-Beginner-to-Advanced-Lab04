9576a1c93b63e4905228ef0262f06ba05dd2ad21
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-16 20:33:10 +0100

    58. Buttons in Android

--- Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.layouts
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var myText : TextView
+    lateinit var doMagic: Button
+    lateinit var myButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,11 +25,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        myText = findViewById(R.id.textEx)
-        myText.setTextColor(Color.RED)
+//        myText = findViewById(R.id.textEx)
+//        myText.setTextColor(Color.RED)
+//
+//        myText.setOnClickListener {
+//            myText.setTextColor(Color.BLUE)
+//        }
 
-        myText.setOnClickListener {
-            myText.setTextColor(Color.BLUE)
+        doMagic = findViewById(R.id.doMagic)
+        myButton = findViewById(R.id.myButton)
+        myText = findViewById(R.id.textEx)
+
+        doMagic.setOnClickListener {
+            doMagic.setBackgroundColor(Color.RED)
+            myText.setText("Magic")
+
+            myText.isVisible = false
+            doMagic.isVisible = false
+        }
+
+        myButton.setOnClickListener {
+            myText.isVisible = true
+            doMagic.isVisible = true
         }
     }
 }
