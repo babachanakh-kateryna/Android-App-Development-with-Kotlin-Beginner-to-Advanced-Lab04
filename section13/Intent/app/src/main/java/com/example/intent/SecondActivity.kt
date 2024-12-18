6c68bc347f199ca438eb224ba367ca371f29d87d
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-19 00:42:18 +0100

    Intent

---
+++ section13/Intent/app/src/main/java/com/example/intent/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.example.intent
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+
+    lateinit var result: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        result = findViewById(R.id.result)
+
+        var userName: String = intent.getStringExtra("name").toString()
+        var userAge: Int = intent.getIntExtra("age", 0)
+
+        result.text = "Your name is $userName and your age is $userAge"
+    }
+}
