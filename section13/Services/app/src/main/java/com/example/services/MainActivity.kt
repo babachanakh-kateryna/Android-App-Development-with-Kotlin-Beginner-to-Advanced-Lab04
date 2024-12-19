28463004a19f25f7eab8859d52cc3436affc4b3b
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-19 02:42:29 +0100

    Receivers

--- section13/Services/app/src/main/java/com/example/services/MainActivity.kt
+++ section13/Services/app/src/main/java/com/example/services/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.services
 
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -10,9 +11,11 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var startClassService: Button
-    lateinit var startIntentService: Button
-    lateinit var stopService: Button
+//    lateinit var startClassService: Button
+//    lateinit var startIntentService: Button
+//    lateinit var stopService: Button
+
+    var br = BroadcastExample()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,23 +27,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        startClassService = findViewById(R.id.startClassicService)
-        startIntentService = findViewById(R.id.startJobIntent)
-        stopService = findViewById(R.id.stopService)
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        this.registerReceiver(br, filter)
 
-        startClassService.setOnClickListener {
-            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
-            startService(intent)
-        }
+//        startClassService = findViewById(R.id.startClassicService)
+//        startIntentService = findViewById(R.id.startJobIntent)
+//        stopService = findViewById(R.id.stopService)
+//
+//        startClassService.setOnClickListener {
+//            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
+//            startService(intent)
+//        }
+//
+//        startIntentService.setOnClickListener {
+//            val intent = Intent(this@MainActivity, JobIntentServiceExemple::class.java)
+//            JobIntentServiceExemple.myBackgroundService(this@MainActivity, intent)
+//        }
+//
+//        stopService.setOnClickListener {
+//            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
+//            stopService(intent)
+//        }
+    }
 
-        startIntentService.setOnClickListener {
-            val intent = Intent(this@MainActivity, JobIntentServiceExemple::class.java)
-            JobIntentServiceExemple.myBackgroundService(this@MainActivity, intent)
-        }
+    override fun onStart() {
+        super.onStart()
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        this.registerReceiver(br, filter)
+    }
 
-        stopService.setOnClickListener {
-            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
-            stopService(intent)
-        }
+    override fun onStop() {
+        super.onStop()
+        this.unregisterReceiver(br)
     }
 }
