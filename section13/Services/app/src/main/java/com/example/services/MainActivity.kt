7f7fc899077c5fcaeed63dc7f88594a1e0a3e407
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-19 02:53:46 +0100

    Android View Binding

--- section13/Services/app/src/main/java/com/example/services/MainActivity.kt
+++ section13/Services/app/src/main/java/com/example/services/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainBinding: ActivityMainBinding
 //    lateinit var startClassService: Button
 //    lateinit var startIntentService: Button
 //    lateinit var stopService: Button
@@ -31,24 +33,29 @@ class MainActivity : AppCompatActivity() {
         filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
         this.registerReceiver(br, filter)
 
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+
+        setContentView(view)
+
 //        startClassService = findViewById(R.id.startClassicService)
 //        startIntentService = findViewById(R.id.startJobIntent)
 //        stopService = findViewById(R.id.stopService)
 //
-//        startClassService.setOnClickListener {
-//            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
-//            startService(intent)
-//        }
-//
-//        startIntentService.setOnClickListener {
-//            val intent = Intent(this@MainActivity, JobIntentServiceExemple::class.java)
-//            JobIntentServiceExemple.myBackgroundService(this@MainActivity, intent)
-//        }
-//
-//        stopService.setOnClickListener {
-//            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
-//            stopService(intent)
-//        }
+        mainBinding.startClassicService.setOnClickListener {
+            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
+            startService(intent)
+        }
+
+        mainBinding.startJobIntent.setOnClickListener {
+            val intent = Intent(this@MainActivity, JobIntentServiceExemple::class.java)
+            JobIntentServiceExemple.myBackgroundService(this@MainActivity, intent)
+        }
+
+        mainBinding.stopService.setOnClickListener {
+            val intent = Intent(this@MainActivity, ClassicServiceExemple::class.java)
+            stopService(intent)
+        }
     }
 
     override fun onStart() {
